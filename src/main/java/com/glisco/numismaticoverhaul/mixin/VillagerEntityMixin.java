96eaea11907797fea7dfa9f7b6fa50808231055d
glisco <glisco03@web.de>
2022-02-06 23:29:19 +0100

    fix #12 by not allowing money bag creating in trade output slots, remove stdout in VillagerEntityMixin

--- src/main/java/com/glisco/numismaticoverhaul/mixin/VillagerEntityMixin.java
+++ src/main/java/com/glisco/numismaticoverhaul/mixin/VillagerEntityMixin.java
@@ -32,7 +32,7 @@ public abstract class VillagerEntityMixin extends MerchantEntity {
                 :
                 0));
 
-        System.out.println("Base reputation " + reputation + ", adjusted " + adjustedReputation);
+//        System.out.println("Base reputation " + reputation + ", adjusted " + adjustedReputation);
 
         this.getOffers().forEach(offer -> ((NumismaticTradeOfferExtensions) offer).numismatic$setReputation(adjustedReputation));
     }
