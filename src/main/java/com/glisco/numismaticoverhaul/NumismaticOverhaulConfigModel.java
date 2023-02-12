91a91fa1ec82db519e4030abf8a0cb2bbd006ad4
Noaaan <noaaan@hotmail.com>
2023-02-12 02:06:03 +0100

    Remove MCA Config as its no longer needed, related to #68, bumps version
    Signed-off-by: Noaaan <noaaan@hotmail.com>

--- src/main/java/com/glisco/numismaticoverhaul/NumismaticOverhaulConfigModel.java
+++ src/main/java/com/glisco/numismaticoverhaul/NumismaticOverhaulConfigModel.java
@@ -6,6 +6,7 @@ import io.wispforest.owo.config.annotation.Modmenu;
 import io.wispforest.owo.config.annotation.Nest;
 import io.wispforest.owo.config.annotation.RestartRequired;
 
+@SuppressWarnings("unused")
 @Modmenu(modId = "numismatic-overhaul")
 @Config(name = "numismatic-overhaul", wrapperName = "NumismaticOverhaulConfig")
 public class NumismaticOverhaulConfigModel {
@@ -13,9 +14,6 @@ public class NumismaticOverhaulConfigModel {
     @RestartRequired
     @Comment("Whether villagers should use Numismatic currency for trading")
     public boolean enableVillagerTrading = true;
-
-    @Comment("Whether taxes from Minecraft Comes Alive: Reborn should be delivered as Numismatic currency")
-    public boolean enableMcaCompatibility = true;
 
     @RestartRequired
     @Comment("Whether Numismatic currency should be injected into the loot tables of loot chests")
