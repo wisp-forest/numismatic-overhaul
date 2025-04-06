927ab15e9a9dfab1b0c829bef7b5e3fe6cee7866
Noaaan <noaaan@hotmail.com>
2025-04-06 18:14:56 +0200

    Fix single-coin tooltips, add some TODOs

--- src/main/java/com/glisco/numismaticoverhaul/mixin/SetTradeOffersS2CPacketMixin.java
+++ src/main/java/com/glisco/numismaticoverhaul/mixin/SetTradeOffersS2CPacketMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.packet.s2c.play.SetTradeOffersS2CPacket;
 import net.minecraft.village.TradeOfferList;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 // TODO: Review, not sure if this is correct. Seems to work?
 @Mixin(SetTradeOffersS2CPacket.class)
 public class SetTradeOffersS2CPacketMixin implements NumismaticTradeOfferExtensions {
+    @Unique
     private int numismatic$reputation = 0;
 
     @Override
