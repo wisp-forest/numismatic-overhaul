e59dceac741e4e5fd2599395c160963c5bb15daf
Noaaan <noaaan@hotmail.com>
2025-06-22 15:56:55 +0200

    Use head injectors instead of overwrites (#166)
    Solves mod compatibility problems

--- src/main/java/com/glisco/numismaticoverhaul/mixin/TradeOfferInternalsMixin.java
+++ src/main/java/com/glisco/numismaticoverhaul/mixin/TradeOfferInternalsMixin.java
@@ -1,5 +1,6 @@
 package com.glisco.numismaticoverhaul.mixin;
 
+import com.glisco.numismaticoverhaul.NumismaticOverhaul;
 import com.glisco.numismaticoverhaul.villagers.data.NumismaticVillagerTradesRegistry;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
@@ -7,43 +8,53 @@ import net.fabricmc.fabric.impl.object.builder.TradeOfferInternals;
 import net.minecraft.village.TradeOffers;
 import net.minecraft.village.VillagerProfession;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
-@Mixin(TradeOfferInternals.class)
+@Mixin(value = TradeOfferInternals.class, priority = 500)
 public class TradeOfferInternalsMixin {
 
     /**
      * @author glisco
      * @reason Redirect Fabric API trade helper to integrate with NO
      */
-    @Overwrite(remap = false)
-    public static synchronized void registerVillagerOffers(VillagerProfession profession, int level, TradeOfferHelper.VillagerOffersAdder factory) {
+    @Inject(method = "registerVillagerOffers", at = @At("HEAD"), cancellable = true, remap = false)
+    private static synchronized void registerVillagerOffers(VillagerProfession profession, int level, TradeOfferHelper.VillagerOffersAdder factory, CallbackInfo ci) {
+        if (!NumismaticOverhaul.CONFIG.enableVillagerTrading()) return;
         final var factories = new ArrayList<TradeOffers.Factory>();
         factory.onRegister(factories, false);
 
         NumismaticVillagerTradesRegistry.registerFabricVillagerTrades(profession, level, factories);
+        ci.cancel();
     }
 
     /**
      * @author glisco
      * @reason Redirect Fabric API trade helper to integrate with NO
      */
-    @Overwrite(remap = false)
-    public static synchronized void registerWanderingTraderOffers(int level, Consumer<List<TradeOffers.Factory>> factory) {
+    @Inject(method = "registerWanderingTraderOffers", at = @At("HEAD"), cancellable = true, remap = false)
+    private static synchronized void registerWanderingTraderOffers(int level, Consumer<List<TradeOffers.Factory>> factory, CallbackInfo ci) {
+        if (!NumismaticOverhaul.CONFIG.enableVillagerTrading()) return;
         final var factories = new ArrayList<TradeOffers.Factory>();
         factory.accept(factories);
 
         NumismaticVillagerTradesRegistry.registerFabricWanderingTraderTrades(level, factories);
+        ci.cancel();
     }
 
     /**
      * @author glisco
      * @reason Disable registering trades completely to make sure that no one ever interferes with the NO system
      */
-    @Overwrite(remap = false)
-    private static void registerOffers(Int2ObjectMap<TradeOffers.Factory[]> leveledTradeMap, int level, Consumer<List<TradeOffers.Factory>> factory) {}
+    @Inject(method = "registerOffers", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void registerOffers(Int2ObjectMap<TradeOffers.Factory[]> leveledTradeMap, int level, Consumer<List<TradeOffers.Factory>> factory, CallbackInfo ci) {
+        if (NumismaticOverhaul.CONFIG.enableVillagerTrading()) {
+            ci.cancel();
+        }
+    }
 }
