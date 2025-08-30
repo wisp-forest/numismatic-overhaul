16fa0056b48e8c57e3485c5ba17847ea237e9a00
Noaaan <noaaan@hotmail.com>
2025-08-30 17:01:16 +0200

    Implement Sell Map Tag Adapter (#165)
    Also updates the target point selection to use appropriate structure tags

--- src/main/java/com/glisco/numismaticoverhaul/villagers/json/adapters/SellMapTagAdapter.java
+++ src/main/java/com/glisco/numismaticoverhaul/villagers/json/adapters/SellMapTagAdapter.java
@@ -5,16 +5,14 @@ import com.glisco.numismaticoverhaul.currency.CurrencyHelper;
 import com.glisco.numismaticoverhaul.villagers.json.TradeJsonAdapter;
 import com.glisco.numismaticoverhaul.villagers.json.VillagerJsonHelper;
 import com.google.gson.JsonObject;
-import io.wispforest.owo.util.RegistryAccess;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.FilledMapItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.item.map.MapIcon;
 import net.minecraft.item.map.MapState;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.tag.StructureTags;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
@@ -22,13 +20,11 @@ import net.minecraft.util.JsonHelper;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradeOffers;
-import net.minecraft.world.gen.structure.StructureKeys;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
 import java.util.Locale;
 
-public class SellMapAdapter extends TradeJsonAdapter {
+public class SellMapTagAdapter extends TradeJsonAdapter {
 
     @Override
     @NotNull
@@ -36,23 +32,23 @@ public class SellMapAdapter extends TradeJsonAdapter {
 
         loadDefaultStats(json, true);
 
-        VillagerJsonHelper.assertString(json, "structure");
+        VillagerJsonHelper.assertString(json, "tag");
         int price = json.get("price").getAsInt();
 
-        final var structure = new Identifier(JsonHelper.getString(json, "structure"));
+        final var structure = new Identifier(JsonHelper.getString(json, "tag"));
         return new Factory(price, structure, max_uses, villager_experience, price_multiplier);
     }
 
     private static class Factory implements TradeOffers.Factory {
         private final int price;
-        private final Identifier structureId;
+        private final Identifier structureTagId;
         private final int maxUses;
         private final int experience;
         private final float multiplier;
 
         public Factory(int price, Identifier feature, int maxUses, int experience, float multiplier) {
             this.price = price;
-            this.structureId = feature;
+            this.structureTagId = feature;
             this.maxUses = maxUses;
             this.experience = experience;
             this.multiplier = multiplier;
@@ -63,16 +59,16 @@ public class SellMapAdapter extends TradeJsonAdapter {
             if (!(entity.getWorld() instanceof ServerWorld serverWorld)) return null;
 
             final var registry = serverWorld.getRegistryManager().get(RegistryKeys.STRUCTURE);
-            final var feature = RegistryAccess.getEntry(registry, this.structureId);
-
-            if (feature == null || feature.getKey().isEmpty()) {
-                NumismaticOverhaul.LOGGER.error("Tried to create map to invalid structure " + this.structureId);
+            final var entryList = registry.getOrCreateEntryList(TagKey.of(RegistryKeys.STRUCTURE, structureTagId));
+            final var features = entryList.stream().toList();
+            if (features.isEmpty()) {
+                NumismaticOverhaul.LOGGER.error("Tried to create map to invalid structure " + this.structureTagId);
                 return null;
             }
 
             final var result = serverWorld.getChunkManager().getChunkGenerator().locateStructure(
                     serverWorld,
-                    RegistryEntryList.of(feature),
+                    RegistryEntryList.of(features.stream().toList()),
                     entity.getBlockPos(),
                     1500,
                     true
@@ -80,6 +76,7 @@ public class SellMapAdapter extends TradeJsonAdapter {
 
             if (result == null) return null;
             final var blockPos = result.getFirst();
+            final var feature = result.getSecond();
 
             var iconType = MapIcon.Type.TARGET_POINT;
             if (feature.isIn(StructureTags.ON_TREASURE_MAPS))
