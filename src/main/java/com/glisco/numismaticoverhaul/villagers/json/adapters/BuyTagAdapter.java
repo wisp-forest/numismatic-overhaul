b00b29311b5c18fa62e91496730f1ff78459e843
Noaaan <noaaan@hotmail.com>
2025-08-30 17:40:38 +0200

    Update adapters from #167 to 1.21

--- src/main/java/com/glisco/numismaticoverhaul/villagers/json/adapters/BuyTagAdapter.java
+++ src/main/java/com/glisco/numismaticoverhaul/villagers/json/adapters/BuyTagAdapter.java
@@ -8,7 +8,6 @@ import com.glisco.numismaticoverhaul.villagers.json.VillagerJsonHelper;
 import com.google.gson.JsonObject;
 import io.wispforest.owo.ops.TextOps;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
@@ -17,8 +16,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.village.TradeOffer;
-import net.minecraft.village.TradeOffers;
+import net.minecraft.village.*;
 import org.jetbrains.annotations.NotNull;
 
 public class BuyTagAdapter extends TradeJsonAdapter {
@@ -31,7 +29,7 @@ public class BuyTagAdapter extends TradeJsonAdapter {
         VillagerJsonHelper.assertJsonObject(json, "buy");
 
         final var buyObject = JsonHelper.getObject(json, "buy");
-        final var tag = new Identifier(JsonHelper.getString(buyObject, "tag"));
+        final var tag = Identifier.of(JsonHelper.getString(buyObject, "tag"));
         final int count = JsonHelper.getInt(buyObject, "count", 1);
 
         int price = json.get("price").getAsInt();
@@ -71,10 +69,9 @@ public class BuyTagAdapter extends TradeJsonAdapter {
                 return null;
             }
             int randomPrice = price;
-            randomPrice += randomPrice * 0.10f * MathHelper.nextFloat(random, 0.4f, 2.0f);
+            randomPrice += (int) (randomPrice * 0.10f * MathHelper.nextFloat(random, 0.4f, 2.0f));
 
-            final var buyStack = new ItemStack(entries.get(random.nextInt(entries.size())).value(), this.count);
-            return new TradeOffer(buyStack, CurrencyHelper.getClosest(randomPrice), this.maxUses, this.experience, multiplier);
+            return new TradeOffer(new TradedItem(entries.get(random.nextInt(entries.size())).value(), this.count), CurrencyHelper.getClosest(randomPrice), this.maxUses, this.experience, multiplier);
         }
     }
 
--- src/main/java/com/glisco/numismaticoverhaul/villagers/json/adapters/SellEnchantedItemAdapter.java
+++ src/main/java/com/glisco/numismaticoverhaul/villagers/json/adapters/SellEnchantedItemAdapter.java
@@ -4,19 +4,21 @@ import com.glisco.numismaticoverhaul.currency.CurrencyHelper;
 import com.glisco.numismaticoverhaul.villagers.json.TradeJsonAdapter;
 import com.glisco.numismaticoverhaul.villagers.json.VillagerJsonHelper;
 import com.google.gson.JsonObject;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.enchantment.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.EnchantmentTags;
 import net.minecraft.util.JsonHelper;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.village.TradeOffer;
-import net.minecraft.village.TradeOffers;
+import net.minecraft.village.*;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Map;
+import java.util.*;
 
 public class SellEnchantedItemAdapter extends TradeJsonAdapter {
 
@@ -57,16 +59,39 @@ public class SellEnchantedItemAdapter extends TradeJsonAdapter {
 
         public TradeOffer create(Entity entity, Random random) {
             ItemStack itemStack = itemToEnchant.copy();
-            itemStack = EnchantmentHelper.enchant(random, itemStack, level, allowTreasure);
-
-            int price = basePrice;
-            for (Map.Entry<Enchantment, Integer> entry : EnchantmentHelper.get(itemStack).entrySet()) {
-                price += price * 0.10f + basePrice * (entry.getKey().isTreasure() ? 2f : 1f) *
-                        entry.getValue() * MathHelper.nextFloat(random, .8f, 1.2f)
-                        * (5f / (float) entry.getKey().getRarity().getWeight());
+            var enchantmentRegistry = entity.getWorld().getRegistryManager().get(RegistryKeys.ENCHANTMENT);
+            var nonTreasureEnchants = enchantmentRegistry.getEntryList(EnchantmentTags.NON_TREASURE);
+            var treasureRegistry = enchantmentRegistry.getEntryList(EnchantmentTags.TRADEABLE);
+            var enchants = List.<EnchantmentLevelEntry>of();
+            if (allowTreasure && treasureRegistry.isPresent()) {
+                enchants = EnchantmentHelper.generateEnchantments(random, itemStack, level, treasureRegistry.get().stream());
+            }
+            else if (nonTreasureEnchants.isPresent()) {
+                enchants = EnchantmentHelper.generateEnchantments(random, itemStack, level, nonTreasureEnchants.get().stream());
             }
 
-            return new TradeOffer(CurrencyHelper.getClosest(price), itemStack, maxUses, this.experience, multiplier);
+            for (EnchantmentLevelEntry enchant : enchants) {
+                itemStack.addEnchantment(enchant.enchantment, enchant.level);
+            }
+
+            int price = basePrice;
+            var enchantments = EnchantmentHelper.getEnchantments(itemStack);
+
+            for (Object2IntMap.Entry<RegistryEntry<Enchantment>> entry : enchantments.getEnchantmentEntries()) {
+                var enchantment = entry.getKey();
+                var isTreasure = enchantment.isIn(EnchantmentTags.TREASURE);
+
+                if (enchantment.isIn(EnchantmentTags.DOUBLE_TRADE_PRICE)) {
+                    price *= 2;
+                }
+
+                price += (int) (price * 0.10f + basePrice * (isTreasure ? 2f : 1f) *
+                    entry.getIntValue() * MathHelper.nextFloat(random, .8f, 1.2f)
+                    * (5f / (float) enchantment.value().getWeight()));
+            }
+
+            var itemAndCost = CurrencyHelper.getClosest(price);
+            return new TradeOffer(new TradedItem(itemAndCost.getItem(), itemAndCost.getCount()), itemStack, maxUses, this.experience, multiplier);
         }
     }
 }
