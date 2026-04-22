552f37fcbdd5ccef0e770a962e2707c589638d1e
Noaaan <noaaan@hotmail.com>
2026-04-22 19:56:36 +0200

    Add config for Purse location in Pawn Shops

--- src/main/java/com/glisco/numismaticoverhaul/client/NumismaticOverhaulClient.java
+++ src/main/java/com/glisco/numismaticoverhaul/client/NumismaticOverhaulClient.java
@@ -107,9 +107,8 @@ public class NumismaticOverhaulClient implements ClientModInitializer {
         Layers.add(
             PurseLayerContainer::new,
             new PurseLayerElement<>((instance, component) -> instance.alignComponentToHandledScreenCoordinates(
-                component,
-                220,
-                50
+                component, NumismaticOverhaul.CONFIG.purseOffsets.pawnShopX(),
+                70 + NumismaticOverhaul.CONFIG.purseOffsets.pawnShopY()
             )),
             PawnShopScreen.class
         );
